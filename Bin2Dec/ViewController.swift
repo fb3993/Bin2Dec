1e584e407b14c31ac0024b4f4846a5de518233d7
fb3993 <fb3993@gmail.com>
2020-10-27 14:45:04 +0100

    Initial Commit

---
+++ Bin2Dec/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bin2Dec
+//
+//  Created by Filippo Bertolina on 27/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
